3310f448338b4185b6135b5ddb533dd1ec475203
AngelMond <abraham_will@hotmail.com>
2022-07-15 23:38:53 -0500

    Created navigationBar position:fixed/ Need to add navBar fixed for mobile devices

---
+++ assets/js/index.js
@@ -0,0 +1,6 @@
+const navigation_bar = document.querySelector('#navigationBar');
+
+
+window.addEventListener('scroll', function(){
+    navigation_bar.classList.toggle('active' , window.scrollY > 95)
+})
